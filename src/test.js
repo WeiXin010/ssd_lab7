6469f6e8000ea947a71feb33f2454bbb78595610
Wei Xin <lwxin87@gmail.com>
2025-07-16 10:54:17 +0800

    testing

--- src/test.js
+++ src/test.js
@@ -1,3 +1,4 @@
  // test.js
  const expression = '1 + 1';
  eval(`console.log(${expression})`); // This evaluates the expression 1 + 1
+ eval(`console.log(${expression})`); // This evaluates the expression 1 + 1
