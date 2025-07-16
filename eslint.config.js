c04f5cfae342ec09f2a02fde479373fc2d89456a
Wei Xin <lwxin87@gmail.com>
2025-07-16 10:06:07 +0800

    added security features

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
  import globals from "globals";
  import pluginJs from "@eslint/js";
  import pluginSecurity from "eslint-plugin-security";
+ import pluginSecurityNode from "eslint-plugin-security-node";
+
  /** @type {import('eslint').Linter.Config[]} */
  export default [
   {files: ["**/*.{js,mjs,cjs,jsx}"]},
@@ -8,10 +10,14 @@
   pluginJs.configs.recommended,
   {
     plugins: {
-      security: pluginSecurity
+      security: pluginSecurity,
+      "security-node": pluginSecurityNode,
+
     },
     rules: {
       ...pluginJs.configs.recommended.rules,
+      ...pluginSecurity.configs.recommended.rules,
+      ...pluginSecurityNode.configs.recommended.rules,
       "security/detect-eval-with-expression": "error",
     }
   }
